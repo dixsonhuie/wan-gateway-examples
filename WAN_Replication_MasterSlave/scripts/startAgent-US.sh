e205fa939843354be9b41bc556ecbccbc3ef0544
dixsonhuie <dixson.huie@gmail.com>
2017-01-31 16:30:44 -0500

    Update startAgent-US.sh

--- WAN_Replication_MasterSlave/scripts/startAgent-US.sh
+++ WAN_Replication_MasterSlave/scripts/startAgent-US.sh
@@ -11,6 +11,6 @@ export EXT_JAVA_OPTIONS="$EXT_JAVA_OPTIONS -Dcom.gigaspaces.start.httpPort=9913"
 export EXT_JAVA_OPTIONS="$EXT_JAVA_OPTIONS -Dcom.gs.zones=US"
 
 # Modify this as needed
-export GSC_JAVA_OPTIONS="-Xmx128m"
+export XAP_GSC_OPTIONS="-Xmx128m"
 
 $GS_HOME/bin/gs-agent.sh gsa.gsm 1 gsa.gsc 2
